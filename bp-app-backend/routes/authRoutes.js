272fb8b11c96625ea9a44f442683f78e6a249ba4
komal <komalchoudhary1134@gmail.com>
2026-06-05 10:04:13 +0530

    Fixed auth routes - working locally

--- bp-app-backend/routes/authRoutes.js
+++ bp-app-backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { signup, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);   // ✅ Change to /signup
-router.post("/login", login);     // ✅ Change to /login
+router.post("/auth/signup", signup);   // ✅ Pehle wala
+router.post("/auth/login", login);     // ✅ Pehle wala
 
 export default router;
